e0a76b7069ff525c4bd526288d9106f8d72b6bcd
Rob Newport <robnewport@gmail.com>
2017-09-05 16:59:14 -0700

    remove example folder

--- Example/Pods/Target Support Files/FlipSnap/FlipSnap-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_FlipSnap : NSObject
-@end
-@implementation PodsDummy_FlipSnap
-@end
--- Example/Pods/Target Support Files/Pods-FlipSnap_Tests/Pods-FlipSnap_Tests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_FlipSnap_Tests : NSObject
-@end
-@implementation PodsDummy_Pods_FlipSnap_Tests
-@end
